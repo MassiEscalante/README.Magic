fa5fda3f9fe6e0ae7eb2f7d69b40ba7a46b75acf
MassiEscalante <massiel2611@gmail.com>
2024-09-25 16:20:08 -0700

    License badge code updated

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,30 +2,33 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== 'None') {
-    return `![License](https://img.shields.io/badge/License-${license.replace(' ', '%20')}-blue.svg)`;;
+    
+    return `![License](https://img.shields.io/badge/License-${license.replace(' ', '%20')}-blue.svg)`; 
   }
-  return '';
+  return ''; 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== 'None') {
-    return `\n* [License](#license)\n`;
+    return `\n* [License](#license)\n`;  
   }
-  return '';
+  return '';  
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== 'None') {
+    
     return `## License\nThis project is licensed under the ${license} license.`;
   }
-  return '';
+  return '';  
 }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  console.log("License selected:", data.license);
   return `# ${data.title}
 
 ${renderLicenseBadge(data.license)}
